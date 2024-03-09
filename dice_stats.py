4859d02d6f234ac78d989c496b212d465972c021
Alessandro Amadori <alexander.amadori@gmail.com>
2024-03-09 19:44:16 +0100

    correct distribution

--- dice_stats.py
+++ dice_stats.py
@@ -21,23 +21,22 @@ def compute_min(*dice):
     return sum([die.min for die in dice])
 
 def compute_max(*dice):
-    return prod([die.faces**die.rolls for die in dice])
+    return sum([die.faces*die.rolls for die in dice]), prod([die.faces**die.rolls for die in dice])
 
 d4 = Die(4, 1)
 min_ = d6.rolls
-min_ = compute_min(d6, d4)
+min_ = compute_min(d6)
 
 
-max__ = compute_max(d6)
+max_, number_different_outputs = compute_max(d6)
 
-max_ = d6.rolls*d6.max
+# max_ = d6.rolls*d6.max
 print(min_)
 print(max_)
-print(max__)
-possible_rolls = []
-all_possible_outcomes={i: 0 for i in range(min_,max_ + 1)}
-print(f"{all_possible_outcomes=}")
+print(number_different_outputs)
 
+
+possible_rolls = []
 for _ in range(d6.rolls):
     possible_rolls.append(d6.values)
 
@@ -49,11 +48,17 @@ res = compute_all_combinations(*possible_rolls)
 
 print(f"{res=}")
 
-for el in res:
-    if el in all_possible_outcomes.keys():
-        all_possible_outcomes[el] =+1
 
-print(f"{all_possible_outcomes=}")
+def count_values(lst):
+    count_dict = {}
+    for value in lst:
+        if value in count_dict:
+            count_dict[value] += 1
+        else:
+            count_dict[value] = 1
+    return count_dict
+
+all_possible_outcomes = count_values(res)
 
 # if __name__ == "__main__":
 #     # print("Number of d2: ")
